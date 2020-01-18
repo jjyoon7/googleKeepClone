edd66f5694754545d210d02f1ce53c049f35d53e
jjyoon7 <jay.yoon7@gmail.com>
2020-01-18 16:31:07 +0100

    '$' infront of the element in the 'constructor' function means it is an html element.

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 class App {
     constructor() {
+        this.$form = document.querySelector("#form");
         this.addEventListerns();
     }
 
     addEventListerns() {
         //initiate all the addEventlisteners
         document.body.addEventListener('click', handleFormClick);
+    }
+
+    handleFormClick() {
+        const isFormClicked = this.$form.contains(event.target)
     }
 }
 
