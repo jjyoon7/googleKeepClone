6b52d9443b4364371bc0679187679de0c679750d
jjyoon7 <jay.yoon7@gmail.com>
2020-01-18 16:41:23 +0100

    you need to use the clickEvents passed function argument as arrow function where you can pass the event and then initiate the function with this. Otherwise the 'target' would return 'undefined'

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 class App {
     constructor() {
         this.$form = document.querySelector("#form");
+        this.$noteTitle = document.querySelector("#note-title");
+        this.$formButtons = document.querySelector("#form-buttons");
+
         this.addEventListerns();
     }
 
     addEventListerns() {
         //initiate all the addEventlisteners
-        document.body.addEventListener('click', handleFormClick);
+        document.body.addEventListener('click', event => {
+            this.handleFormClick();
+        });
     }
 
-    handleFormClick() {
+    handleFormClick(event) {
         const isFormClicked = this.$form.contains(event.target);
         if(isFormClicked) {
             this.openForm();
         } else {
-            //close form
+            this.closeForm();
         }    
     }
 
-    openForm() {
+    openForm(){
         this.$form.classList.add('form-open');
+        this.$noteTitle.style.display = 'block';
+        this.$formButtons.style.display = 'block'; 
+    }
+
+    closeForm() {
+        this.$form.classList.remove('form-open');
+        this.$noteTitle.style.display = 'none';
+        this.$formButtons.style.display = 'none'; 
     }
 }
 
