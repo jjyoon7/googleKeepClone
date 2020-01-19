4f679cf9edd803a80c01e88b9d2211907115128d
jjyoon7 <jay.yoon7@gmail.com>
2020-01-19 20:49:27 +0100

    add displayNotes()

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ class App {
     constructor() {
         this.notes = [];
 
+        this.$placeholder = document.querySelector('#placeholder')
         this.$form = document.querySelector("#form");
         this.$noteTitle = document.querySelector("#note-title");
         this.$noteText = document.querySelector("#note-text");
@@ -58,7 +59,12 @@ class App {
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         };
         this.notes = [...this.notes, newNote];
-        console.log(this.notes);
+        this.displayNotes();
+    }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0;
+        hasNotes ? this.$placeholder.style.display = 'none' : this.$placeholder.style.display = 'flex';
     }
 }
 
